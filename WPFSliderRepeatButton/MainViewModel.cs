a9b1ef1ea49c49798161386c6c95cb9aabf94f93
yunasim <ynsim@solidit.co.kr>
2024-02-20 23:47:16 +0900

    #✨feature: Silder와 RepeatButton 구현 완료

---
+++ WPFSliderRepeatButton/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WPFSliderRepeatButton
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int _value;
+
+        public MainViewModel()
+        {
+            Value = 50;
+        }
+
+        [RelayCommand]
+        private void Increase()
+        {
+            Value++;
+        }
+
+        [RelayCommand]
+        private void Decrease()
+        {
+            Value--;
+        }
+    }
+}
